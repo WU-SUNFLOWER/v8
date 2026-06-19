729b270ea5951a4e74f9fec1c6fc378309b30576
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2026-06-19 13:17:16 +0800

    首批调试代码

---
+++ learn_v8/demo-string.js
@@ -0,0 +1,6 @@
+function demoStringOps(a, b) {
+    let result = a + b;
+    return result;
+}
+
+demoStringOps("Hello","World!");
---
+++ learn_v8/script-with-functions.js
@@ -0,0 +1,15 @@
+function foo() {
+    print("Hello, I'm foo");
+}
+
+function bar() {
+    print("Hello, I'm bar");
+}
+
+print("before foo()");
+foo();
+print("after foo()");
+
+print("before bar()");
+bar();
+print("after bar()");
